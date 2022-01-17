b04c43f961ec7af72564ce49e70c151ebabb5c59
Ivan Zhukov <zhkvivan@gmail.com>
2022-01-17 12:20:38 +0000

    Client, server have been created. Create-react-app, routing, starting components and styles - header, button - were created

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+	return (
+		<>
+			<Routes>{/* <Route path="/" element={} /> */}</Routes>
+			<Header />
+		</>
+	);
+}
+
+export default App;
